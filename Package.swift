2698618196f7b5553a1d1b5d65b49c2a757f4efa
Eric Russell <cire1776@gmail.com>
2025-04-04 17:13:16 -0400

    Remove dependence on swift collections for now.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0"),
         .package(url: "https://github.com/Quick/Quick.git", from: "7.0.0"),
         .package(url: "https://github.com/Quick/Nimble.git", exact: "13.0.0"),
