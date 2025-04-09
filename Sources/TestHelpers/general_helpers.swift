ce0b340bca929aaa6aa105195e868389e9594797
Eric Russell <cire1776@gmail.com>
2025-04-09 15:02:49 -0400

    Add comprehension test harness methods to general_helpers.

--- Sources/TestHelpers/general_helpers.swift
+++ Sources/TestHelpers/general_helpers.swift
@@ -5,6 +5,7 @@
 //  Created by Eric Russell on 2/28/25.
 //
 import Foundation
+@testable import MultitaskingEngine
 
 func waitForCondition(_ condition: @escaping () -> Bool, timeout: TimeInterval = 0.5, interval: TimeInterval = 0.01) async {
     let maxRetries = Int(timeout / interval)
@@ -17,4 +18,46 @@ func waitForCondition(_ condition: @escaping () -> Bool, timeout: TimeInterval =
     print("⚠️ Timeout reached waiting for condition.")
 }
 
+func createPreinitLint(value: Any?, executionContext: StreamExecutionContext) -> Lint {
+    return { [executionContext] _ in
+        executionContext.ensure("input", defaultValue: value)
+        executionContext.ensure("comma_delimited_line", defaultValue: "~nil~")
+        return .running }
+}
 
+func simplePreinit(_ stream: String = "input", default: Any = "~nil~") -> (StreamExecutionContext) -> Lint {
+    return { ctx in
+        return { _ in
+            ctx.ensure(stream, defaultValue: `default`)
+            return .running
+        }
+    }
+}
+
+func preinits(_ streams:[String: Any?]) -> (StreamExecutionContext) -> Lint {
+    return { context in
+        return { _ in
+            streams.forEach { stream, `default` in
+                context.ensure(stream, defaultValue: `default`)
+            }
+            return .running
+        }
+    }
+}
+
+func runComprehension<Comp, T>(
+    input: Any,
+    blueprint: (StreamExecutionContext) -> Comp,
+    with contextActions: (StreamExecutionContext) throws -> (preinit: Lint, extract: ()->T?)
+) async -> T? where Comp: Comprehension.Subscription {
+    let context = SubscriptionStreamExecutionContext()
+    
+    let result = try? contextActions(context)
+    let comp = blueprint(context)
+    
+    let instance = comp.instantiate(preinitialization_lint: result?.preinit, executionContext: context)
+    let runner = ManualLintRunner(provider: instance)
+    _ = await runner.executeAll()
+    
+    return result?.extract()
+}
